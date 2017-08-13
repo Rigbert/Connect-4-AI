49fc6cd3af5d3376f0ac9c812b9c12d0e2f17a54
Rigbert <drigby@k-state.edu>
2017-08-13 16:31:11 -0500

    Updated

--- ai.py
+++ ai.py
@@ -1,32 +1,147 @@
+import random
+import copy
+
+
 class Board:
-    def __init__(self,current_player):
+    def __init__(self, current_player=0, p_board=None):
         self.current_player = current_player
-        self.bit_board = '0' * 64
-        self.board = [['.' for i in range(7)] for j in range(6)]
-        pass
+        if not p_board:
+            self.hash_key = 0
+            self.hash_board = self.initial_hash()
+            self.bit_board = int('0' * 64, 2)
+            self.mask = int('0' * 64, 2)
+            self.board = [['.' for col in range(7)] for row in range(6)]
+            self.moves_remaining = 42
+            self.col_heights = [0 for col in range(7)]
+            self.last_played_col = None
+        else:
+            self.bit_board = p_board.bit_board
+            self.board = p_board.board
+            self.current_player = p_board.current_player
+            self.hash_board = p_board.hash_board
+            self.hash_key = p_board.hash_key
+            self.moves_remaining = p_board.moves_remaining
+            self.col_heights = p_board.col_heights
+            self.last_played_col = p_board.last_played_col
+            self.mask = p_board.mask
 
     def __hash__(self):
-        pass
+        return self.hash_key
 
-    def can_play(self,col):
-        pass
+    def __eq__(self, other):
+        return True if self.bit_board == other.bit_board and self.moves_remaining == other.moves_remaining and \
+                       self.mask == other.mask else False
 
-    def play(self,col):
+    def __str__(self):
+        t = ""
+        for i in reversed(self.board):
+            for j in i:
+                t+=j
+            t+='\n'
+        t+='\n'
+        return t
+
+    @classmethod
+    def copy_board(cls, prev_board):
+        return Board(prev_board.current_player, prev_board)
+
+    def initial_hash(self):
+        board = [[random.getrandbits(64) for i in range(2)] for j in range(42)]
+        return board
+
+    def can_play(self, col):
+        if self.col_heights[col] != 6:
+            return True
+        return False
+
+    def play(self, col):
         if self.can_play(col):
-            pass
+            self.col_heights[col] += 1
+            self.last_played_col = col
+            piece = 'x' if self.get_current_player() else 'o'
+            self.board[self.col_heights[col]-1][col] = piece
+            # finds it in hashing based on 42 - weird numerics in the 42 section
+            self.hash_key ^= \
+                self.hash_board[self.last_played_col * 6 +
+                                self.col_heights[self.last_played_col] - 1][self.get_current_player()]
+            self.bit_board ^= self.mask
+            self.mask |= self.mask + self.bottom_mask(col)
+            self.moves_remaining -= 1
 
         else:
-            raise 'Invalid Move'
-        self.current_player = 1 if self.current_player == 0 else 0
+            raise ValueError('Cannot play that move -- check before action')
+        self.current_player = 1 if self.get_current_player() == 0 else 0
+
+    def bottom_mask(self, col):
+        return 1 << col*7
+
+    def is_winning_move(self,col):
+        pos = self.bit_board
+        pos |= (self.mask + self.bottom_mask(col)) & self.column_mask(col)
+        m = pos & (pos >> 7)
+        if m & (m >> (2*7)):
+            return True
+        m = pos & (pos >> 6)
+        if m & (m >> (2 * 6)):
+            return True
+        m = pos & (pos >> (6 + 2))
+        if m & (m >> (2 * (6 + 2))):
+            return True
+        m = pos & (pos >> 1)
+        if m & (m >> 2):
+            return True
+        return False
+
+    def won_game(self):
+        pos = self.bit_board
+        m = pos & (pos >> 7)
+        if m & (m >> (2 * 7)):
+            return True
+        m = pos & (pos >> 6)
+        if m & (m >> (2 * 6)):
+            return True
+        m = pos & (pos >> (6 + 2))
+        if m & (m >> (2 * (6 + 2))):
+            return True
+        m = pos & (pos >> 1)
+        if m & (m >> 2):
+            return True
+        return False
+
+    def column_mask(self, col):
+        return (1 << 6) - 1 << col*7
+
+    def get_current_player(self):
+        return self.current_player
+
+
 
 
 class AI:
-    def __init__(self, board_state):
-        pass
+    def __init__(self):
+        self.z_positions = {}
+        self.column_order = [3, 4, 2, 5, 1, 6, 0]
+        self.best_move = None
 
-if __name__ == '__main__':
-    newBoard = Board(1)
-    for i in newBoard.board:
-        for j in i:
-            print(j, end='')
-        print()
+    def negamax(self, board_state, alpha, beta):
+        if board_state.moves_remaining == 0:
+            return 0
+
+        for i in range(7):
+            if board_state.can_play(self.column_order[i]) and board_state.is_winning_move(self.column_order[i]):
+                return (49 - abs(board_state.moves_remaining-42))//2
+        best_value = -float('inf')
+        for i in range(7):
+            if board_state.can_play(self.column_order[i]):
+                new_board_state = Board.copy_board(board_state)
+                new_board_state.play(self.column_order[i])
+                score = -self.negamax(new_board_state, -beta, -alpha)
+
+                best_value = max(score, best_value)
+                alpha = max(score, alpha)
+                if alpha >= beta:
+                    break
+
+        return best_value
+
+
--- game.py
+++ game.py
@@ -1,1 +1,17 @@
 from ai import Board, AI
+
+artInt = AI()
+cur_board = Board(0)
+print("Welcome to Connect-4 AI")
+while not cur_board.won_game():
+    print(cur_board)
+    print("Please make a move based on a zero-indexed column: ")
+    user_input = int(input())
+    while user_input > 6 or user_input < 0 or not cur_board.can_play(user_input):
+        print("Please enter a valid option")
+        user_input = int(input())
+    cur_board.play(user_input)
+    print(artInt.negamax(cur_board, -float("inf"), float("inf")))
+
+
+
